13d7c6178665a54f50b65326d68683a94fb6eb69
HoJuHy <dhwld7392@naver.com>
2024-09-13 10:26:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ if __name__ == '__main__':
     pn.is_prime_number(13)
     pn.is_prime_number(24)
     pn.is_prime_number(31)
-    pn.is_prime_number(45)
-    pn.is_prime_number(97)
+
+for i in range(10):
+    print(i, end='')
